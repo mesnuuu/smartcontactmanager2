6c17ed2c35677b3ae20bf1924a46a175ef772cfb
mesnuuu <mesnuuu@gmail.com>
2021-09-27 20:18:34 +0500

    01 - Database Configuration - Entities
    Database Setup.
    
    Entitities and DB Tables.
    
    Successful storage in Db using controller.
    
    ## #40 done ##

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String test() {
+		
+		User user = new User();
+		user.setName("John Doe");
+		user.setEmail("seconedemaiol@email.com");
+		
+		userRepository.save(user);
+		
+		return "Working... !! " ;
+	}
+}
