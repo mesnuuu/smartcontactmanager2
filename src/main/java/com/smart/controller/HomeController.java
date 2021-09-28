6c6f7c016897bc1e7018b023d8ad28a0de3096ec
mesnuuu <mesnuuu@gmail.com>
2021-09-28 09:02:43 +0500

    02- Setting up Bootstrap and Templates
    #41

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
@@ -11,20 +13,43 @@ import com.smart.entities.User;
 @Controller
 public class HomeController {
 
-	@Autowired
-	private UserRepository userRepository;
+	/*
+	 * @Autowired private UserRepository userRepository;
+	 */
 	
 	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		
-		User user = new User();
-		user.setName("John Doe");
-		user.setEmail("seconedemaiol@email.com");
-		
-		userRepository.save(user);
-		
-		return "Working... !! " ;
+	@RequestMapping("/base")
+	public String base() {
+		return "base";
 	}
+	
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		
+		model.addAttribute("title", "Home - SMC");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		
+		model.addAttribute("title", "About - SMC");
+		return "about";
+	}
+	
+	
+	/*
+	 * @GetMapping("/test")
+	 * 
+	 * @ResponseBody public String test() {
+	 * 
+	 * User user = new User(); user.setName("John Doe");
+	 * user.setEmail("seconedemaiol@email.com");
+	 * 
+	 * userRepository.save(user);
+	 * 
+	 * return "Working... !! " ; }
+	 */
+	
 }
